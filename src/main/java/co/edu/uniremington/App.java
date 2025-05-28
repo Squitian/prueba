18fbfc683a8a06856ac133611b40bc682bf82496
Santiago <correo@gmail.com>
2025-05-27 20:21:17 -0500

    actualizacion

--- src/main/java/co/edu/uniremington/App.java
+++ src/main/java/co/edu/uniremington/App.java
@@ -15,29 +15,11 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
         int a = 10;
         int  b = 5;
         int restar = a-b;
         System.out.println(restar);
-=======
-        int num1 = 5;
-        int num2 = 7;
-        int suma = num1 + num2;
 
-        System.out.println("La suma de " + num1 + " y " + num2 + " es: " + suma);
-
-       int a = 2;
-       int b = 4;
-       int suma = a+b;
-        System.out.println(suma);
-
-       int c = 10;
-       int d = 12;
-       int mult = c*d;
-        System.out.println(mult);
-
->>>>>>> 6da4eb5d68d17ef783d7934cf1800dd0f222a446
     }
 
 }

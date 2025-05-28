04b5c3a5d88c7e42119d7ae31b3a655c6cfc7422
Juan Garcia <manujuan498@gmail.com>
2025-05-27 20:29:25 -0500

    Codigo else if

--- src/main/java/co/edu/uniremington/App.java
+++ src/main/java/co/edu/uniremington/App.java
@@ -1,9 +1,9 @@
 package co.edu.uniremington;
-<<<<<<< HEAD
 
 import org.w3c.dom.ls.LSOutput;
 
 import javax.swing.*;
+import java.time.LocalTime;
 
 /**
  * Hello world!
@@ -18,6 +18,21 @@ public class App
         int restar = a-b;
         System.out.println(restar);
 
+        LocalTime hora = LocalTime.now();
+        String saludo;
+
+        if (hora.getHour() < 12) {
+            saludo = "¡Buenos días!";
+        } else if (hora.getHour() < 18) {
+            saludo = "¡Buenas tardes!";
+        } else {
+            saludo = "¡Buenas noches!";
+        }
+
+        System.out.println(saludo + " ¡Bienvenido al mundo de la programación!");
     }
+
+
+}
 
 }

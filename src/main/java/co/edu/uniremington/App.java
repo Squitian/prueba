8c9da7c3159d8f011e7bccb6230c269ef207f0c3
Juan Garcia <manujuan498@gmail.com>
2025-05-27 20:05:25 -0500

    Suma de 5 y 7

--- src/main/java/co/edu/uniremington/App.java
+++ src/main/java/co/edu/uniremington/App.java
@@ -1,17 +1,22 @@
 package co.edu.uniremington;
-
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int num1 = 5;
+        int num2 = 7;
+        int suma = num1 + num2;
+
+        System.out.println("La suma de " + num1 + " y " + num2 + " es: " + suma);
+
        int a = 2;
        int b = 4;
        int suma = a+b;
         System.out.println(suma);
     }
+
 }

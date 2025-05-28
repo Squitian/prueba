5967fa2e29dba914f45697409dab20d2093412ae
Juan Garcia <manujuan498@gmail.com>
2025-05-27 20:27:41 -0500

    Codigo con else y if

--- src/main/java/co/edu/uniremington/App.java
+++ src/main/java/co/edu/uniremington/App.java
@@ -1,14 +1,8 @@
 package co.edu.uniremington;
-<<<<<<< HEAD
-
 import org.w3c.dom.ls.LSOutput;
-
 import javax.swing.*;
+import java.time.LocalTime;
 
-/**
- * Hello world!
- *
- */
 public class App
 {
     public static void main( String[] args )
@@ -18,6 +12,22 @@ public class App
         int restar = a-b;
         System.out.println(restar);
 
+        LocalTime hora = LocalTime.now();
+        String saludo;
+
+        if (hora.getHour() < 12) {
+            saludo = "¡Buenos días!";
+        } else if (hora.getHour() < 18) {
+            saludo = "¡Buenas tardes!";
+        } else {
+            saludo = "¡Buenas noches!";
+        }
+
+        System.out.println(saludo + " ¡Bienvenido al mundo de la programación!");
     }
+
+
+
+}
 
 }

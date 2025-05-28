587e7f92727df3515ec2e33e5910ca10d3f2f6a9
Santiago <correo@gmail.com>
2025-05-27 20:14:38 -0500

    multiplicaciÃon

--- src/main/java/co/edu/uniremington/App.java
+++ src/main/java/co/edu/uniremington/App.java
@@ -1,5 +1,9 @@
 package co.edu.uniremington;
 
+import org.w3c.dom.ls.LSOutput;
+
+import javax.swing.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int a = 2;
+        int  b = 4;
+        int multiplicar = a*b;
+        System.out.println(multiplicar);
     }
 }
